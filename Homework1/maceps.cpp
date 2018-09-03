7cfd2cbf56cc2918a69c6385d9d3089ca21cf63c
KensieCarr <35310005+KensieCarr@users.noreply.github.com>
2018-09-03 16:59:38 -0600

    Update maceps.cpp

--- Homework1/maceps.cpp
+++ Homework1/maceps.cpp
@@ -3,9 +3,11 @@
 using namespace std;
 
 void machineEpsilon(float EPS);
+void doubleMachineEpsilon(double EPS);
 
 int main() {
   machineEpsilon(0.5);
+  doubleMachineEpsilon(0.5);
   return 0;
 }
 
@@ -16,4 +18,13 @@ void machineEpsilon(float EPS) {
     EPS /= 2;
   }
   cout << "Machine Epsilon is: " << prev_epsilon << endl;
+}
+
+void doubleMachineEpsilon(double EPS) {
+  double prev_epsilon;
+  while ((1 + EPS) != 1) {
+    prev_epsilon = EPS;
+    EPS /= 2;
+  }
+  cout << "Double Precision Machine Epsilon is: " << prev_epsilon << endl;
 }
