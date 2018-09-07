d5c21e13645c857071fbaabbb1016473d4048d2e
KensieCarr <35310005+KensieCarr@users.noreply.github.com>
2018-09-07 11:39:39 -0600

    Update and rename maceps.cpp to dmaceps.cpp

--- Homework1/dmaceps.cpp
+++ Homework1/dmaceps.cpp
@@ -1,23 +1,11 @@
 #include <iostream>
-#include <cfloat>
 using namespace std;
 
-void machineEpsilon(float EPS);
 void doubleMachineEpsilon(double EPS);
 
 int main() {
-  machineEpsilon(0.5);
   doubleMachineEpsilon(0.5);
   return 0;
-}
-
-void machineEpsilon(float EPS) {
-  float prev_epsilon;
-  while ((1 + EPS) != 1) {
-    prev_epsilon = EPS;
-    EPS /= 2;
-  }
-  cout << "Machine Epsilon is: " << prev_epsilon << endl;
 }
 
 void doubleMachineEpsilon(double EPS) {
