3eb723bdf5e7096b43a560831e53cf53f8a884e5
KensieCarr <35310005+KensieCarr@users.noreply.github.com>
2018-12-15 00:00:30 -0700

    Update Problem8.py

--- Homework2/Problem8.py
+++ Homework2/Problem8.py
@@ -9,6 +9,18 @@ def g(x):
     #sin(pi x)
     return sin(pi*x)
 
+def bisectionMethod(fn, a, b, tol):
+    mid = (a+b)/2
+    while (b-a)/2 > tol:
+        if fn(mid) == 0:
+            return mid
+        elif fn(a)*fn(mid) < 0:
+            b = mid
+        else:
+            a = mid
+        mid = (a+b)/2
+    return mid
+
 def newtonMethod(fn, x0, tol, maxIter):
     x1 = fn(x0)
     for i in range(0, maxIter):
@@ -23,5 +35,5 @@ def newtonMethod(fn, x0, tol, maxIter):
     
 if __name__ == '__main__':
     x = symbols('x')
-    print(newtonMethod(f, 1.8, 1.0e-5, 100))
-    print(newtonMethod(g, 2.2, 1.0e-5, 100))
+    print(newtonMethod(f, bisectionMethod(f, 1, 3, 1.0e-1), 1.0e-5, 100))
+    print(newtonMethod(g, bisectionMethod(g, 1.25, 2.5, 1.0e-1), 1.0e-5, 100))
